b3a5ff8d9617a61fa2904f9d4c0797bdf5465674
Samikizilboga <samikizilboga@finesci.com>
2023-08-13 22:23:23 +0300

    last version

---
+++ sami_kizilboga_codes/enocaProject/jpa-example/src/main/java/com/example/jpa/dto/OrderRequest.java
@@ -0,0 +1,16 @@
+package com.example.jpa.dto;
+
+import com.example.jpa.entity.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OrderRequest {
+
+    private Customer customer;
+}
